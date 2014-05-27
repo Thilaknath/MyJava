c3e11f58b84f5b7e5e1a91d56ea11dcf0ee858fc
Thilaknath <Thilaknath@192.168.2.18>
2014-05-27 16:48:56 -0400

    My first commit

---
+++ FirstTime/src/Application1.java
@@ -0,0 +1,13 @@
+
+public class Application1 {
+
+	public static void main(String[] args) {
+		int value = 0;
+		while (value < 10) {
+			System.out.println("Hello World " +  value);
+
+			value = value + 1;
+		}
+	}
+
+}
